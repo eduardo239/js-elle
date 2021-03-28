ce596f5d3d26c243815bb8e3969d650e50efc1a4
eduardo239 <eucrieiumaconta@gmail.com>
2021-03-28 10:52:26 -0300

    adicionado shrink na navigation e modal no btn play

--- js/index.js
+++ js/index.js
@@ -10,3 +10,45 @@ function show_modal() {
     modal.classList.add('show');
   }
 }
+// play
+const button_show_play = document.getElementById('show_play');
+const button_close_play = document.getElementById('close_play');
+const modal_play = document.getElementById('modal_play');
+
+function show_modal_play() {
+  const x = modal_play.classList.contains('close_play');
+  if (x) {
+    modal_play.classList.remove('close_play');
+  } else {
+    modal_play.classList.add('close_play');
+  }
+}
+
+button_show_play.addEventListener('click', show_modal_play());
+
+// shrink navbar
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  console.log(document.documentElement.scrollTop);
+
+  const links = document.querySelectorAll('.menu a');
+  const navigation = document.getElementById('navigation');
+
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    console.log(links);
+    links.forEach((e) => (e.style.fontSize = '.9rem'));
+    navigation.style.backgroundColor = '#fff';
+    navigation.style.height = '36px';
+    navigation.style.boxShadow =
+      '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)';
+  } else {
+    navigation.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
+    links.forEach((e) => (e.style.fontSize = '1rem'));
+    navigation.style.height = 'auto';
+    navigation.style.boxShadow =
+      '0 1px 1px rgba(0, 0, 0, 0.06), 0 1px 1px rgba(0, 0, 0, 0.08)';
+  }
+}
